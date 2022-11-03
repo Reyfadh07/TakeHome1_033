612cd4213a54824d674fa05d3cb1786bbf3ac4d4
Reyfadh07 <raihan.farmasi@gmail.com>
2022-11-03 13:09:51 +0700

    commit pertama

---
+++ src/main/java/ws/a/learntakehome_33/Learntakehome33Application.java
@@ -0,0 +1,13 @@
+package ws.a.learntakehome_33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learntakehome33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learntakehome33Application.class, args);
+	}
+
+}
