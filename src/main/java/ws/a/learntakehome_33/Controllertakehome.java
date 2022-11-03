88d5af0c2b23884d97c0cf2114bdfabbb826222e
Reyfadh07 <raihan.farmasi@gmail.com>
2022-11-03 13:53:28 +0700

    menambahkan code pada class controller

--- src/main/java/ws/a/learntakehome_33/Controllertakehome.java
+++ src/main/java/ws/a/learntakehome_33/Controllertakehome.java
@@ -5,10 +5,39 @@
  */
 package ws.a.learntakehome_33;
 
+import java.io.IOException;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author raiha
  */
+@Controller
 public class Controllertakehome {
+    
+    @RequestMapping(value = "/tampilan")
+    public String tampil(
+            @RequestParam(value = "nama") String isinama,
+            @RequestParam(value = "lok") String isilok,
+            @RequestParam(value = "gbr") MultipartFile isigbr,
+            Model bawa
+            
+    ) throws IOException{
+        byte[] img = isigbr.getBytes();
+        String base64gbr = Base64.encodeBase64String(img);
+        String gbrlink = "data:image/*;base64,".concat(base64gbr);
+        
+        bawa.addAttribute("paketnama", isinama);
+        bawa.addAttribute("paketlok", isilok);
+        bawa.addAttribute("paketgbr", gbrlink);
+        
+    
+        return "view";
+    }
     
 }
